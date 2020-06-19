148f2c1d59e917ba2a73540063b59bc3145c5652
Bao Luong <Bao@Baos-MacBook-Pro.local>
2020-06-18 20:58:37 -0400

    Resized images for web

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
 import './App.css';
-import baby from './images/baby.png';
-import cara from './images/cara.png';
-import charlie from './images/charlie.png';
-import city from './images/city.png';
-import coffee from './images/coffee.png';
-import cow from './images/cow.png';
-import elephant from './images/elephant.png';
-import eyes from './images/eyes.png';
-import fish from './images/fish.png';
-import girl from './images/girl.png';
-import glasses from './images/glasses.png';
-import kingfish from './images/kingfish.png';
-import koi from './images/koi.png';
-import lake from './images/lake.png';
-import lana from './images/lana.png';
-import meadow from './images/meadow.png';
-import net from './images/net.png';
-import onion from './images/onion.png';
-import pumpkin from './images/pumpkin.png';
-import rafa from './images/rafa.png';
-import skittles from './images/skittles.png';
-import sky from './images/sky.png';
-import smile from './images/smile.png';
-import spartan from './images/spartan.png';
-import guy from './images/guy.png';
-import tomato from './images/tomato.png';
-import vdp from './images/vdp.png';
+import cara from './images/cara.jpg';
+import charlie from './images/charlie.jpg';
+import city from './images/city.jpg';
+import coffee from './images/coffee.jpg';
+import cow from './images/cow.jpg';
+import elephant from './images/elephant.jpg';
+import eyes from './images/eyes.jpg';
+import fish from './images/fish.jpg';
+import ghost from './images/ghost.jpg';
+import girl from './images/girl.jpg';
+import glasses from './images/glasses.jpg';
+import kingfish from './images/kingfish.jpg';
+import koi from './images/koi.jpg';
+import lake from './images/lake.jpg';
+import lana from './images/lana.jpg';
+import meadow from './images/meadow.jpg';
+import net from './images/net.jpg';
+import onion from './images/onion.jpg';
+import pumpkin from './images/pumpkin.jpg';
+import rafa from './images/rafa.jpg';
+import skittles from './images/skittles.jpg';
+import sky from './images/sky.jpg';
+import smile from './images/smile.jpg';
+import spartan from './images/spartan.jpg';
+import guy from './images/guy.jpg';
+import tomato from './images/tomato.jpg';
+import vdp from './images/vdp.jpg';
 
 function App() {
   return (
@@ -54,16 +54,14 @@ function App() {
         <div class="card big" id="elephant" style={{backgroundImage: `url(${elephant})`}} />
         <div class="card tall" id="guy" style={{backgroundImage: `url(${guy})`}} />
         <div class="card small" id="rafa" style={{backgroundImage: `url(${rafa})`}} />
-
         <div class="card small" id="tomato" style={{backgroundImage: `url(${tomato})`}} />
         <div class="card wide" id="lake" style={{backgroundImage: `url(${lake})`}} />
         <div class="card small" id="eyes" style={{backgroundImage: `url(${eyes})`}} />
-        
         <div class="card tall" id="lana" style={{backgroundImage: `url(${lana})`}} />
         <div class="card small" id="vdp" style={{backgroundImage: `url(${vdp})`}} />
         <div class="card big" id="girl" style={{backgroundImage: `url(${girl})`}} />
         <div class="card small" id="spartan" style={{backgroundImage: `url(${spartan})`}} />
-        <div class="card small" id="baby" style={{backgroundImage: `url(${baby})`}} />
+        <div class="card small" id="ghost" style={{backgroundImage: `url(${ghost})`}} />
         <div class="card small" id="net" style={{backgroundImage: `url(${net})`}} />
         <div class="card small" id="pumpkin" style={{backgroundImage: `url(${pumpkin})`}} />
       </div>
